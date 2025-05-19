9a7456f79b31df200b9e3560cff76e58a47c6beb
topalovic236 <topalovicnikola45@gmail.com>
2025-05-19 22:09:39 +0200

    Modified server.js

--- server/server.js
+++ server/server.js
@@ -12,11 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: [
-      "https://imagify-3-19xs.onrender.com",
-      "http://localhost:5173" 
+      'https://imagifya.netlify.app',
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
+    methods: ["GET", "POST", "PUT", "DELETE"]
   }));
 
 await connectDB(); 
